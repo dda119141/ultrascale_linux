6453ca6b3004a327df542ffc6cc1382ef72f0e16
Maxime Moge <dda119141@gmail.com>
2025-01-20 15:33:40 +0100

    Added pmu-rom download logic - OS can boot from shell script

--- qemu/run_qemu_fsbl_qspi_release.sh
+++ qemu/run_qemu_fsbl_qspi_release.sh
@@ -6,21 +6,12 @@ export this_dir=$(cd `dirname $0` ; pwd)
 readonly machine="zcu102-zynqmp"
 readonly xilinxdir="${this_dir}/.."
 readonly drives="${this_dir}/../drives"
-readonly generated="${xilinxdir}/../yocto/xilinx/zcu102/generated"
-#readonly qemu_xilinx="${generated}/tmp/work/x86_64-linux/qemu-xilinx-helper-native/1.0-r1/recipe-sysroot-native/usr/bin"
-readonly qemu_xilinx="${this_dir}/qemu_src/qemu_xilinx/build"
-readonly qemu_cmd="${qemu_xilinx}/usr/bin/qemu-system-microblazeel"
-readonly deploy_folder="${generated}/tmp/deploy/images/${machine}"
-readonly hw_dtb_top="${deploy_folder}/qemu-hw-devicetrees/zcu102-arm.dtb"
-readonly pmu_dtb="${deploy_folder}/qemu-hw-devicetrees/zynqmp-pmu.dtb"
-#readonly atf="${deploy_folder}/arm-trusted-firmware.elf"
+readonly qemu_xilinx="${this_dir}/generated/src/build"
+readonly pmu_dtb="${xilinxdir}/bootloader/pmu/generated/src/LATEST/MULTI_ARCH/zynqmp-pmu.dtb"
 readonly atf="${xilinxdir}/bootloader/arm_trusted_firmware/generated/src/build/zynqmp/release/bl31/bl31.elf"
-readonly pmu_rom="${deploy_folder}/pmu-rom.elf"
-readonly pmu_firmware="${deploy_folder}/pmu-firmware-zcu102-zynqmp.elf"
-readonly uboot="${deploy_folder}/u-boot.elf"
-readonly uboot_dtb="${deploy_folder}/u-boot.dtb"
-readonly hw_dtb="${xilinxdir}/kernel/device-tree/zcu102-arm.dtb"
-readonly fsbl="${xilinxdir}/bootloader/coreboot/build/fsboot_a53_zc102.elf"
+readonly pmu_rom="${xilinxdir}/bootloader/pmu/generated/PMU_ROM/pmu-rom.elf"
+readonly hw_dtb="${xilinxdir}/bootloader/pmu/generated/src/LATEST/MULTI_ARCH/zcu102-arm.dtb"
+readonly fsbl="${xilinxdir}/bootloader/coreboot/generated/src/build/fsboot_a53_zc102.elf"
 readonly QSPI_24BIT="1"
 readonly QSPI_24BIT_INDEX="0"
 readonly QSPI_32BIT="2"
@@ -35,7 +26,7 @@ touch ${qemu_shmdir}/qemu-rport-_pmu@0
 
 sudo ${qemu_xilinx}/qemu-system-microblazeel -M microblaze-fdt \
 	-display none \
-	-hw-dtb ${deploy_folder}/qemu-hw-devicetrees/multiarch/zynqmp-pmu.dtb \
+	-hw-dtb ${pmu_dtb} \
 	-kernel ${pmu_rom} \
 	-machine-path ${qemu_shmdir} \
 &
